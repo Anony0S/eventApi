109895ff80390c0bdf94a81846f6615c6f98ecb7
Anony <ljx1583861@qq.com>
2022-07-03 21:30:35 +0800

    修改说明文件

--- db/index.js
+++ db/index.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'admin123',
-    database: 'my_db_01',
+    password: 'password',
+    database: 'DateBaseName',
 })
 
 // 暴露出去
